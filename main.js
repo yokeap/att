330f7c78583b135a5c6508666453817f70e44c20
Siwakorn Sukprasertchai <siwakorn.ictrl@gmail.com>
2019-04-05 17:39:55 +0700

    switch to ffi

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 'use strict'
-var ffi = require('ffi');
-var val = 0
-    , tempVal = 0
+var  ffi = require('ffi')
+    , SerialPort = require('serialport');
+
+var   tempVal = 0
+    , value = 0
     , tubeCount = 0
     , barcodeDelay = 0;
 
@@ -11,8 +13,8 @@ var lib = ffi.Library('./src/libAttControl', {
     'process_write': [ 'int', ['int', 'int', 'int']]
 });
 
-var flagReadyA = false
-    , flagReadyB = false
+var flagHomeA = false
+    , flagHomeB = false
     , flagShootA = false
     , flagShootB = false;
 
@@ -46,51 +48,68 @@ var I001 = 0x0001
     , O013 = 0x1000
     , O014 = 0x2000;
 
+var M3port = new SerialPort('/dev/ttyUSB0', {
+    autoOpen: false,
+    baudRate: 9600,
+    highWaterMark: 65535
+//parser: SerialPort.parsers.readline('\n'),
+});
 
-setInterval(function(){
-    process();
+function writeKunbus(valProcess){
+    lib.writeData(70, 2, valProcess);
+    console.log(valProcess);
+}
+
+function readKunbus(){
+    lib.writeData(70, 2, valProcess);
+    console.log(valProcess);
+}
+
+
+setInterval(async function(){
+    // let valKunbus = readKunbus();
+    // let valProcess = process(lib.readData(0), ~lib.readData(70));
+    let valProcess = await process();
+    let test = await writeKunbus(valProcess);
+    // lib.writeData(70, 2, process());
 }, 10);
 
-
 function process(){
-    var value = 0
-        , tempMask = 0;
-
-    val = lib.readData(0);
-    tempVal = lib.readData(70);
-     console.log(val);
-    if(val & I011){				//Sensor-A detected
+    // lib.readData(0) = lib.readData(0);
+    // ~lib.readData(70) = ~lib.readData(70);
+    if(lib.readData(0) & I011){				//Sensor-A detected
         value |= O003;			//Conveyer-A activate
         tubeCount++;
+        // console.log("tube: " + tubeCount);
     }
-    if(val & I009){				//Sweeper-sensor detected
+    if(lib.readData(0) & I009){				//Sweeper-sensor detected
         value |= O010;			//sweeper activated.
         value |= O011;
-        // printf("%x", tempVal);
+        // printf("%x", ~lib.readData(70));
         // fflush(stdout);
-        if((tempVal & O003) == O003) {
+        if((~lib.readData(70) & O003) == O003) {
             value ^= O003;				//Conveyer-A deactivate.
         }
     }
-    if(val & I012){				//conveyer-B detected
-        if((tempVal & O010) == O010) value ^= O010;			//sweeper deactivate.
-        if((tempVal & O011) == O011) value ^= O011;
+    if(lib.readData(0) & I012){				//conveyer-B detected
+        if((~lib.readData(70) & O010) == O010) value ^= O010;			//sweeper deactivate.
+        if((~lib.readData(70) & O011) == O011) value ^= O011;
         value |= O005;			//conveyer-B activate.
     }
 
     // barcode reader process;
-    if((val & I008) && (flagReadyA)){
-        if((tempVal & O005) == O005) value ^= O005;			//conveyer-B deactivate.
+    if((lib.readData(0) & I008) && (!flagShootA)){
+        if((~lib.readData(70) & O005) == O005) value ^= O005;			//conveyer-B deactivate.
         value |= O009;
         barcodeDelay++;			//tube rolling
         // when cannot read (temporary the fixing is still need.)
         if(barcodeDelay > 300){								//rolling sensor detected
             barcodeDelay = 0;
-            //if((tempVal & O009) == O009) value ^= O009;			//conveyer-B deactivate.
-            value ^= O009;			//rolling-B deactivate.
-            writeData(0x46, 2, value);
+            //if((~lib.readData(70) & O009) == O009) value ^= O009;			//conveyer-B deactivate.
+            value &= 0xEFFF;			//rolling-B deactivate.
+            lib.writeData(70, 2, value);
             flagShootA = true;
-            flagReadyA = false;
+            flagHomeA = false;
         }
     }
     //when barcode success read
@@ -104,56 +123,76 @@ function process(){
     //shooting-A process
     if(flagShootA){
         value |= O001;				//conveyer-C start
-        if((tempVal & O009) == O009) value ^= O009;			//conveyer-B deactivate.
-        if((val & I001) != I001){
+        // if((~lib.readData(70) & O009) == O009) value ^= O009;			//conveyer-B deactivate.
+        if((lib.readData(0) & I001) != I001){
             //shooting-A activate
             value |= O012;
             value |= O006;
         }
         else{
-            if((tempVal & O012) == O012) value ^= O012;
-            if((tempVal & O006) == O006) value ^= O006;
-            flagShootA = false;
+            if((~lib.readData(70) & O012) == O012) value ^= O012;
+            if((~lib.readData(70) & O006) == O006) value ^= O006;
+            flagShootA = false
+            flagHomeA = true;
         }
     }
     //tempus shooting process
-    if((val & I007) && (flagReadyB)) {				//tube shooting rail detected
-        if((tempVal & O001) == O001) value ^= O001;				//conveyer-C stop
-        if(val & I013){				//tempus ready
+    if((lib.readData(0) & I007) && (!flagShootB)) {				//tube shooting rail detected
+        if((~lib.readData(70) & O001) == O001) value ^= O001;				//conveyer-C stop
+        if(lib.readData(0) & I013){				//tempus ready
             flagShootB = true;
+            flagHomeB = false;
         }
     }
 
     //shooting-B process
     if(flagShootB){
-        if((val & I003) != I003){
+        if((lib.readData(0) & I003) != I003){
             value |= O008;
         }
         else{
-            if((tempVal & O008) == O008) value ^= O008;
+            if((~lib.readData(70) & O008) == O008) value ^= O008;
             flagShootB = false;
-            flagReadyB = false;
+            flagHomeB = true;
         }
     }
 
     // Shooting-A Homing
-    if(((val & I002) != I002) && (!flagShootA))	{
+    if(((lib.readData(0) & I002) != I002) && flagHomeA)	{
         value |= O012;		//shooting-A state recoil.
     }
     else{
-        if((tempVal & O012) == O012) value ^= O012;//shooting-A state home position.
-        flagReadyA = true;
+        if(!flagShootA) if((~lib.readData(70) & O012) == O012) value ^= O012;//shooting-A state home position.
+        if((~lib.readData(70) & O009) == O009) value ^= O009;
+        if(flagHomeA) flagHomeA = false;
     }
 
+    // if((lib.readData(0) & I002) && (!flagShootB)){
+    //     if((~lib.readData(70) & O012) == O012) value ^= O012;//shooting-A state home position.
+    //     flagHomeA = true;
+    // }
+    // else{
+    //     value |= O012;
+    // }
+
     // Shooting-B Homing
-    if(((val & I004) != I004) && (!flagShootB))	{
+    if(((lib.readData(0) & I004) != I004) && flagHomeB)	{
         value |= O008|O004;		//shooting-A state recoil.
     }
     else {
-        if((tempVal & O008) == O008) value ^= O008;
-        if((tempVal & O004) == O004) value ^= O004;
-        flagReadyB = true;
+        if(!flagShootB) if((~lib.readData(70) & O008) == O008) value ^= O008;
+        if((~lib.readData(70) & O004) == O004) value ^= O004;
+        if(flagHomeB) flagHomeB = false;
     }
-    // console.log(value);
-    lib.writeData(70, 2, value);
+
+    // if((lib.readData(0) & I004) && (!flagShootB)){
+    //     if((~lib.readData(70) & O008) == O008) value ^= O008;
+    //     if((~lib.readData(70) & O004) == O004) value ^= O004;
+    //     flagHomeB = true;
+    // }
+    // else{
+    //     value |= O008|O004;		//shooting-A state recoil.
+    // }
+
+    return value;
 }
